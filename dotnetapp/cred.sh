dedd292dcccf244a0d698ded0a87c5b9597c55a7
DevOpsKiruthi <102915675+DevOpsKiruthi@users.noreply.github.com>
2024-07-09 17:32:54 +0530

    Add files via upload

---
+++ dotnetapp/cred.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export AZURE_CLIENT_ID="814b4bf0-7950-46b4-a5e1-ad22b5e6d1f6"
+export AZURE_CLIENT_SECRET="uTX8Q~~oZa7xg9mdT28Hp2rg3WarvsOUAEVTJaud"
+export AZURE_TENANT_ID="38723e4f-6e87-4945-b844-2dfd53cabd17"
+export AZURE_SUBSCRIPTION_ID="614ad733-4d6f-4b45-8c9a-0812b67403f7"
+
+
+# Your script logic goes here
